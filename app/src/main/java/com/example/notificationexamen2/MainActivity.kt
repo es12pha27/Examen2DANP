93ee2be0afe380cbd8cd76e838da8ce1fa3e0518
gustavo123.ght69 <gustavo123.ght69@hotmail.com>
2022-07-10 15:19:48 -0500

    whit topic

--- app/src/main/java/com/example/notificationexamen2/MainActivity.kt
+++ app/src/main/java/com/example/notificationexamen2/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.notificationexamen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful){
+                    Log.e("FCM Notify", "ERROR REGISTRANDO TOKEN", task.exception)
+                    return@OnCompleteListener
+                }
+                val token: String? = task.result
+                Log.e("FCM Notify", token, task.exception)
+                Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+
+            })
+        Firebase.messaging.subscribeToTopic("exam2")
+            .addOnCompleteListener { task ->
+                var msg = "Subscribed"
+                if (!task.isSuccessful) {
+                    msg = "Subscribe failed"
+                }
+                Log.d("MainActivity", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/notificationexamen2/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/notificationexamen2/MyFirebaseMessagingService.kt
@@ -1,6 +1,55 @@
 package com.example.notificationexamen2
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+    companion object {
+        private const val TAG = "FCM Notification"
+        const val DEFAULT_NOTIFICATION_ID = 0
+    }
+
+    override fun onNewToken(token: String) {
+        Log.e(TAG, "token: $token")
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        initNotificationChannel(notificationManager)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        var title = message.notification?.title
+        var body = message.notification?.body
+
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        var notificationBuilder = if (Build.VERSION_CODES.O <= Build.VERSION.SDK_INT) {
+            NotificationCompat.Builder(applicationContext, "1")
+        } else {
+            NotificationCompat.Builder(applicationContext)
+        }
+
+        notificationBuilder = notificationBuilder
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+        initNotificationChannel(notificationManager)
+        notificationManager.notify(DEFAULT_NOTIFICATION_ID, notificationBuilder.build())
+
+    }
+
+
+    private fun initNotificationChannel(notificationManager: NotificationManager){
+        if(Build.VERSION_CODES.O <= Build.VERSION.SDK_INT){
+            var channel = notificationManager.getNotificationChannel("1")
+            if(channel == null){
+                channel = NotificationChannel("1", "Default", NotificationManager.IMPORTANCE_DEFAULT)
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+    }
 }
